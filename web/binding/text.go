bb98e67dc0f4d855db44f9be7f4fc18c28c45cb4
951453324 <951453324@qq.com>
2021-06-08 09:49:28 +0800

    fix(web\binding): change text to html

--- web/binding/text.go
+++ web/binding/text.go
@@ -10,7 +10,7 @@ type textBinding struct{}
 
 //Render
 func(textBinding)Render(code int,w http.ResponseWriter,value interface{})error{
-	writeContentType(w,[]string{"application/text; charset=utf-8"})
+	writeContentType(w,[]string{"application/html; charset=utf-8"})
 	w.WriteHeader(code)
 	str:=value.(string)
 	_,err:=w.Write([]byte(str))
