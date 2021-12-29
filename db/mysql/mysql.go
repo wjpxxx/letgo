2177d233d489884d95b82093f5cffa216b794351
WJPone <474790700@qq.com>
2021-12-29 09:09:04 +0800

    fix(mysql):缺少where关键字

--- db/mysql/mysql.go
+++ db/mysql/mysql.go
@@ -399,7 +399,7 @@ func (t *Table) SelectByHasWhere(fields string, where string,hasWhere bool, wher
 		if hasWhere&&len(whereParams)>0{
 			sql=fmt.Sprintf("select %s from %s where %s", fields,t.tableName, where)
 		}else{
-			sql=fmt.Sprintf("select %s from %s %s", fields,t.tableName, where)
+			sql=fmt.Sprintf("select %s from %s where %s", fields,t.tableName, where)
 		}
 	}else{
 		sql=fmt.Sprintf("select %s from %s", fields,t.tableName)
